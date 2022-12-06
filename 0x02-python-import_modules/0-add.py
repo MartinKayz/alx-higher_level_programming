438345f2a1d04f5590faa1e7fd421281be20a7d2
MartinKayz <kubonamartinyafesi132@gmail.com>
2022-12-06 15:06:27 +0300

    Added the __main__ semantic

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-import add_0 as ad
-a = 1
-b = 2
-result = ad.add(a, b)
-print("{} + {} = {}\n".format(a, b, result))
+if __name__ == '__main__':
+
+    import add_0 as ad
+    a = 1
+    b = 2
+    result = ad.add(a, b)
+    print("{} + {} = {}\n".format(a, b, result))
