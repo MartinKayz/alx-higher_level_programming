c0c1eaababa9ac454474031281f9e3f2197c4aa7
MartinKayz <kubonamartinyafesi132@gmail.com>
2022-12-06 15:10:57 +0300

    Made correct import

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    import add_0 as ad
+    from add_0 import add
     a = 1
     b = 2
-    result = ad.add(a, b)
+    result = add(a, b)
     print("{:d} + {:d} = {:d}\n".format(a, b, result))
