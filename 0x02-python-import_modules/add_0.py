114cc097caf7d725cbfc9639b02bc18cc0cf4bbe
MartinKayz <kubonamartinyafesi132@gmail.com>
2022-12-06 15:00:18 +0300

    Made file executable

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+
+if __name__ == "__main__":
+    add()
